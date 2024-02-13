3907e6f74eb74592beb3cc4d6af66d24fee9701d
Maciej Rozpedek <maciek.rozpedek@gmail.com>
2024-02-13 17:42:35 +0100

    Add medal service. Configure local MySQL database

---
+++ src/main/java/com/macroz/medalnetserver/repository/MedalRepository.java
@@ -0,0 +1,11 @@
+package com.macroz.medalnetserver.repository;
+
+import com.macroz.medalnetserver.model.Medal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MedalRepository extends JpaRepository<Medal, Long> {
+
+    Optional<Medal> findMedalById(Long id);
+}
---
+++ src/main/java/com/macroz/medalnetserver/service/MedalService.java
@@ -0,0 +1,40 @@
+package com.macroz.medalnetserver.service;
+
+import com.macroz.medalnetserver.exception.MedalNotFoundException;
+import com.macroz.medalnetserver.model.Medal;
+import com.macroz.medalnetserver.repository.MedalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MedalService {
+    private final MedalRepository medalRepository;
+
+    @Autowired
+    public MedalService(MedalRepository medalRepository) {
+        this.medalRepository = medalRepository;
+    }
+
+    public Medal addMedal(Medal medal) {
+        return medalRepository.save(medal);
+    }
+
+    public List<Medal> findAllMedals() {
+        return medalRepository.findAll();
+    }
+
+    public Medal updateMedal(Medal medal) {
+        return medalRepository.save(medal);
+    }
+
+    public Medal findMedalById(Long id) {
+        return medalRepository.findMedalById(id)
+                .orElseThrow(() -> new MedalNotFoundException("Medal by id " + id + " was not found"));
+    }
+
+    public void deleteMedal(Long id) {
+        medalRepository.deleteById(id);
+    }
+}
